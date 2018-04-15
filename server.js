5839713fb673c510d740acfba8f572a418ab459f
douglas sangster <douglas@an-email.home>
2018-04-15 01:34:53 +0100

    added in a line to say which types of headers were acceptable

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const port  = process.env.PORT || 3000
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://fierce-woodland-96129.herokuapp.com/")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next()
 })
 
