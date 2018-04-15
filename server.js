640262e280248b676a7dd08b28785841484f4853
douglas sangster <douglas@an-email.home>
2018-04-15 01:57:41 +0100

    without the next() part it appears to stall the site, uncommented that line, but leaving middle line stating what headers are allowed still commented out to check if that is required

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const port  = process.env.PORT || 3000
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://fierce-woodland-96129.herokuapp.com")
   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  // next()
+  next()
 })
 
 // app.use(cors({
