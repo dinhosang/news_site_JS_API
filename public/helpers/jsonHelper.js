3087ee44ecab868203e57793fae7e95860234cae
douglas sangster <douglas@an-email.home>
2018-04-15 01:45:56 +0100

    checking logs I noticed that the jsonhelper was hard coded to go to localhost:3000, trying to make it dynamic to allow it to go to heroku web url instead if it exists

--- public/helpers/jsonHelper.js
+++ public/helpers/jsonHelper.js
@@ -1,10 +1,16 @@
 const JsonHelper = function() {
+  this.url = null
+  if(process.env.PORT){
+    this.url = "https://fierce-woodland-96129.herokuapp.com"
+  } else {
+    this.url = "http://localhost:3000"
+  }
 
 }
 
 JsonHelper.prototype.makeRequest = function(url, object, header) {
   const request   = new XMLHttpRequest()
-  const serverUrl = `http://localhost:3000/api/${header}?url=${url}`
+  const serverUrl = `${this.url}/api/${header}?url=${url}`
 
   request.open("GET", serverUrl)
   request.addEventListener('load', this.convertJsonToJs.bind(request, object))
