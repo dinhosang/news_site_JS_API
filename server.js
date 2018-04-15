91a3068e7a6349de802dabe59265c56259c26795
douglas sangster <douglas@an-email.home>
2018-04-15 01:56:20 +0100

    previous attempt without middleware now works, now removing the extra lines to see if only one is necessary

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const port  = process.env.PORT || 3000
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://fierce-woodland-96129.herokuapp.com")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  // next()
 })
 
 // app.use(cors({
