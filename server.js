770860be8b499eef830cca75b3401ebe392552c1
douglas sangster <douglas@an-email.home>
2018-04-15 01:55:14 +0100

    working now, now testing previous methods to run CORS without the module to see if failure was due to the hard-coded localhost url

--- server.js
+++ server.js
@@ -14,17 +14,17 @@ if(!process.env.newsKey){
 
 const port  = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://fierce-woodland-96129.herokuapp.com")
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-//   next()
-// })
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://fierce-woodland-96129.herokuapp.com")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+})
 
 // app.use(cors({
 //   origin: 'https://fierce-woodland-96129.herokuapp.com'
 // }))
 
-app.use(cors())
+// app.use(cors())
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '/public/html/index.html'))
