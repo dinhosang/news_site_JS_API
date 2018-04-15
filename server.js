e362060fb672685794f209af781c4b264d3f08a8
douglas sangster <douglas@an-email.home>
2018-04-15 01:02:51 +0100

    changed require at top of server.js to now only grab value of newsKey file only if site is being run locally and not on heroku

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const app     = express()
 const path    = require('path')
 const request = require('request')
 
-const newsKey   = require('./serverSide/keys/newsKey')
-const port = process.env.PORT || 3000
+let newsKey
+if(!process.env.newsKey){
+  newsKey   = require('./serverSide/keys/newsKey')
+}
+
+const port  = process.env.PORT || 3000
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '/public/html/index.html'))
